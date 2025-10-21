3188d339b1c0979829f7ed2d2305c0797b53947a
Anwesa Ghosh <anwesa2021@gmail.com>
2025-10-21 21:52:45 +0530

    add

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainBanner from '../components/MainBanner.jsx'
+import { assets } from '../assets/assets.js'
+
+const Home = () => {
+  return (
+    <div className='mt-10'>
+        <MainBanner />
+    </div>
+  )
+}
+
+export default Home
