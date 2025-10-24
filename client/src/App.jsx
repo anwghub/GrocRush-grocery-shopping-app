730b1f15a87c62d488a31d63128e06e5b5b2b9f2
Anwesa Ghosh <anwesa2021@gmail.com>
2025-10-24 11:13:56 +0530

    user page done all

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,11 +11,13 @@ import ProductCategory from './pages/ProductCategory';
 import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
 import AddAddress from './pages/AddAddress';
+import MyOrders from './pages/MyOrders';
+import SellerLogin from './components/seller/SellerLogin';
 
 const App = () => {
 
   const isSellerPath = useLocation().pathname.includes("seller");
-  const { showUserLogin } = useAppContext();
+  const { showUserLogin, isSeller } = useAppContext();
 
   return (
     <div>
@@ -31,6 +33,8 @@ const App = () => {
           <Route path='/products/:category/:id' element={<ProductDetails />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/add-address' element={<AddAddress />} />
+          <Route path='/my-orders' element={<MyOrders />} />
+          <Route path='/seller' element={isSeller ? null : <SellerLogin />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer/>} 
---
+++ client/src/pages/seller/SellerLayout.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useAppContext } from '../../context/AppContext';
+import { assets } from '../../assets/assets';
+
+const SellerLayout = () => {
+
+    const {setIsSeller}= useAppContext();
+
+    const sidebarLinks = [
+        { name: "Add Product", path: "/seller", icon: assets.add_icon },
+        { name: "Product List", path: "/seller/product-list", icon: assets.product_list_icon },
+        { name: "Orders", path: "/seller/orders", icon: chaticon },
+    ];
+
+    return (
+        <>
+            <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white transition-all duration-300">
+                <a href="https://prebuiltui.com">
+                    <img className="h-9" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoColored.svg" alt="dummyLogoColored" />
+                </a>
+                <div className="flex items-center gap-5 text-gray-500">
+                    <p>Hi! Admin</p>
+                    <button className='border rounded-full text-sm px-4 py-1'>Logout</button>
+                </div>
+            </div>
+            <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
+                {sidebarLinks.map((item, index) => (
+                    <a href={item.path} key={index}
+                        className={`flex items-center py-3 px-4 gap-3 
+                            ${index === 0 ? "border-r-4 md:border-r-[6px] bg-indigo-500/10 border-indigo-500 text-indigo-500"
+                                : "hover:bg-gray-100/90 border-white text-gray-700"
+                            }`
+                        }
+                    >
+                        {item.icon}
+                        <p className="md:block hidden text-center">{item.name}</p>
+                    </a>
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default SellerLayout
